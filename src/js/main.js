33c235096ae9b7dda52c1529534eccca743976c5
Mohsen jalali <mohsnjli.dev@gmail.com>
2022-10-26 02:12:15 +0330

    Dark Mode added

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,10 @@
 // --------- Go To Top Button -----
 
+
 let goToTop = document.getElementById("goToTop");
 goToTop.style.transform = "translateX(-100px)";
 window.onscroll = function () {
-
     scrollFunction()
-
 };
 
 function scrollFunction() {
@@ -20,3 +19,30 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome , Firefox and Opera
 };
+// --------- Go To Top Button End-----
+
+let header = document.getElementById("header");
+window.onload = function () {
+    headerTrasition()
+}
+
+function headerTrasition() {
+    header.style.transform = "translateY(0)"
+}
+
+// ----------Dark Mode Functions------
+
+let darkModeBtn = document.getElementById("darkModeBtn");
+let darkModeIcon = document.getElementById("darkModeIcon");
+let body = document.querySelector("#body");
+let isDark = true;
+darkModeBtn.addEventListener("click", function () {
+    body.classList.toggle("dark")
+    if (isDark) {
+        darkModeIcon.style.transform = "rotate(180deg)"
+        isDark = false;
+    } else {
+        darkModeIcon.style.transform = "rotate(0)";
+        isDark = true;
+    }
+});
