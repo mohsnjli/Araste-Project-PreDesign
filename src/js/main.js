5fa186aedc4ded622d08dec20fbadab3c87508b1
Mohsen jalali <mohsnjli.dev@gmail.com>
2022-10-29 15:30:28 +0330

    some changes like mobile menu responsive added

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,29 @@
-// --------- Go To Top Button -----
+// ----------Dark Mode Functions------
+let footerWaveSvg = document.getElementById("footerWave");
+let darkModeBtn = document.getElementById("darkModeBtn");
+let darkModeIcon = document.getElementById("darkModeIcon");
+
+let body = document.querySelector("#body");
+let isDark = true;
 
 
+darkModeBtn.addEventListener("click", function () {
+    body.classList.toggle("dark")
+    if (isDark) {
+        footerWaveSvg.src = "/pics/icons/svg/pattern(dark).svg";
+        darkModeIcon.style.transform = "rotate(180deg)";
+        body.style.backgroundColor = "#334e68";
+        darkModeIcon.src = "/pics/icons/svg/contrast(dark).svg";
+        isDark = false;
+    } else {
+        footerWaveSvg.src = "/pics/icons/svg/pattern(footer).svg";
+        darkModeIcon.style.transform = "rotate(0)";
+        body.style.backgroundColor = "#fff";
+        darkModeIcon.src = "/pics/icons/svg/contrast.svg";
+        isDark = true;
+    }
+});
+// --------- Go To Top Function -----
 let goToTop = document.getElementById("goToTop");
 goToTop.style.transform = "translateX(-100px)";
 window.onscroll = function () {
@@ -19,37 +42,59 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome , Firefox and Opera
 };
-// --------- Go To Top Button End-----
 
-let header = document.getElementById("header");
-window.onload = function () {
-    headerTrasition()
-}
+// ------------hamburgerMenuBtn Toggle Function -------
 
-function headerTrasition() {
-    header.style.transform = "translateY(0)"
-}
+let hamburgerMenuBtn = document.getElementById("humburgerMenuBtn");
+let exBtn = document.getElementById("exBtn");
+let navBar = document.querySelector("#navBar");
+hamburgerMenuBtn.addEventListener("click", function () {
+    navBar.style.display = "block";
+    navBar.style.transform = "translateX(0px)"
+})
+exBtn.addEventListener("click", function () {
+    navBar.style.transform = "translateX(-100%)"
+    navBar.style.display = "hidden";
+})
 
-// ----------Dark Mode Functions------
-let waveSvg = document.getElementById("footerWave");
-let darkModeBtn = document.getElementById("darkModeBtn");
-let darkModeIcon = document.getElementById("darkModeIcon");
-let afterLi = document.getElementsByClassName("after");
-let body = document.querySelector("#body");
-let isDark = true;
 
-darkModeBtn.addEventListener("click", function () {
-    body.classList.toggle("dark")
-    if (isDark) {
-        waveSvg.src = "/pics/icons/svg/pattern(dark).svg";
-        darkModeIcon.style.transform = "rotate(180deg)";
-        afterLi.style.backgroundColor = "white";
-        isDark = false;
-    } else {
-        waveSvg.src = "/pics/icons/svg/pattern(footer).svg";
-        darkModeIcon.style.transform = "rotate(0)";
-        afterLi.style.backgroundColor = "white";
 
-        isDark = true;
-    }
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let scrollCont = document.getElementById("srollContainer");
+
+// function nextFunc() {
+//     scrollCont.scrollBy({
+//         left: -40,
+//         behavior: "smooth",
+//     })
+// }
+
+// function prevFunc() {
+//     scrollCont.scrollBy({
+//         left: 80,
+//         behavior: "smooth",
+//     })
+// }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,40 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   darkMode: ["class"],
   theme: {
+
     extend: {
+      gradientColorStops: {
+        "darkBlue": {
+          50: "#f0f4f8",
+          100: "#d9e2ec",
+          200: "#bcccdc",
+          300: "#9fb3c8",
+          400: "#829ab1",
+          500: "#627d98",
+          600: "#486581",
+          700: "#334e68",
+          800: "#243b53",
+          900: "#102a43",
+        },
+      },
       backgroundColor: {
-        "dark-mainBlue": "#102a43",
-        "dark-primaryBlue": "#334e68",
-        "dark-secondaryBlue": "#486581",
+        "darkBlue": {
+          50: "#f0f4f8",
+          100: "#d9e2ec",
+          200: "#bcccdc",
+          300: "#9fb3c8",
+          400: "#829ab1",
+          500: "#627d98",
+          600: "#486581",
+          700: "#334e68",
+          800: "#243b53",
+          900: "#102a43",
+        },
       },
       colors: {
-        "textPurple": "#344675",
+        "txt": "#007acc"
       },
+
       borderRadius: {
         "3xl": "3rem",
       },
@@ -19,9 +44,15 @@ module.exports = {
         "iconW": "30px",
       },
       height: {
-        "iconH" : "30px",
+        "iconH": "30px",
       },
     },
   },
-  plugins: [],
+
+  plugins: [
+    // ...
+    require('tailwind-scrollbar')({
+      nocompatible: true
+    }),
+  ],
 }
